27f4eef1369f33550c4ce3e27122b0746840300b
stereosteam <moabid75@gmail.com>
2021-04-01 01:16:41 -0400

    initialized timits dataset for wav2vec model

--- load_timits.py
+++ load_timits.py
@@ -1,10 +1,17 @@
 from datasets import load_dataset, load_metric
-timit = load_dataset("timit_asr")
-print(timit)
 from datasets import ClassLabel
+from transformers import Wav2Vec2CTCTokenizer
 import random
 import pandas as pd
+import re
+import json
 from IPython.display import display, HTML
+
+timit = load_dataset("timit_asr")
+chars_to_ignore_regex = '[\,\?\.\!\-\;\:\"]'
+# print(timit)
+
+timit = timit.remove_columns(["phonetic_detail", "word_detail", "dialect_region", "id", "sentence_type", "speaker_id"])
 
 def show_random_elements(dataset, num_examples=10):
     assert num_examples <= len(dataset), "Can't pick more elements than there are in the dataset."
@@ -18,5 +25,36 @@ def show_random_elements(dataset, num_examples=10):
     df = pd.DataFrame(dataset[picks])
     display(HTML(df.to_html()))
 
+
+def remove_special_characters(batch):
+    batch["text"] = re.sub(chars_to_ignore_regex, '', batch["text"]).lower()
+    return batch
+
+timit = timit.map(remove_special_characters)
+
+def extract_all_chars(batch):
+  all_text = " ".join(batch["text"])
+  vocab = list(set(all_text))
+  return {"vocab": [vocab], "all_text": [all_text]}
+
 show_random_elements(timit["train"].remove_columns(["file"]))
 
+vocabs = timit.map(extract_all_chars, batched=True, batch_size=-1, keep_in_memory=True, remove_columns=timit.column_names["train"])
+
+# enumerated dictionary creation through union of all distinct letters
+vocab_list = list(set(vocabs["train"]["vocab"][0]) | set(vocabs["test"]["vocab"][0]))
+
+vocab_dict = {v: k for k, v in enumerate(vocab_list)}
+#print(vocab_dict)
+
+vocab_dict["|"] = vocab_dict[" "]
+del vocab_dict[" "]
+
+vocab_dict["[UNK]"] = len(vocab_dict)
+vocab_dict["[PAD]"] = len(vocab_dict)
+#print(len(vocab_dict))
+
+with open('vocab.json', 'w') as vocab_file:
+    json.dump(vocab_dict, vocab_file)
+    
+tokenizer = Wav2Vec2CTCTokenizer("./vocab.json", unk_token="[UNK]", pad_token="[PAD]", word_delimiter_token="|")
